cddcc2d1cefbc53bd4f3ca045b6ecd646dae35ab
Dimasaditya <140600420+consolelog122@users.noreply.github.com>
2024-06-15 03:08:14 +0700

    logic for input massage main.js

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+// responsive hamburger menu
 const menuToggle = document.querySelector(".menu-toggle input");
 const nav = document.querySelector("nav ul");
 
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
+});
+
+// dom
+const buttonSendMassage = document.getElementById("send-massage");
+const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email");
+const inputMessage = document.getElementById("message");
+
+buttonSendMassage.addEventListener("click", () => {
+  if (!inputEmail.includes("@gmail.com")) {
+    return;
+  }
+
+  inputEmail.value = "";
+  inputName.value = "";
+  inputMessage.value = ""; 
 });
